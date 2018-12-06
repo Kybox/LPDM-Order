4040b31ee4745feb2f74a9e3c649733ae5451633
Kybox <nslr@riseup.net>
2018-12-06 15:41:18 +0100

    Add unique index to payment.label column

--- docker/db/sql/1_lpdm_public_payment.sql
+++ docker/db/sql/1_lpdm_public_payment.sql
@@ -4,5 +4,6 @@ CREATE TABLE public."payment"
     id integer DEFAULT nextval('pay_id_seq'::regclass) PRIMARY KEY NOT NULL,
     label VARCHAR (20) NOT NULL
 );
+CREATE UNIQUE INDEX payment_label_uindex ON public.payment (label);
 INSERT INTO public."payment" (id, label) VALUES (1, 'BANKCARD');
 INSERT INTO public."payment" (id, label) VALUES (2, 'PAYPAL');
