1dcd727193b2daee316b646e88d062217d2fd252
Kybox <nslr@riseup.net>
2019-01-12 13:18:14 +0100

    Hotfix PdfServiceImpl class remove useless imports conflict

--- src/main/java/com/lpdm/msorder/service/impl/PdfServiceImpl.java
+++ src/main/java/com/lpdm/msorder/service/impl/PdfServiceImpl.java
@@ -9,7 +9,6 @@ import com.lpdm.msorder.model.OrderedProduct;
 import com.lpdm.msorder.model.User;
 import com.lpdm.msorder.service.OrderService;
 import com.lpdm.msorder.service.PdfService;
-import javafx.scene.layout.Border;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,6 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
