09f8c8e06f412e7b42eb8b2720acd6d01b6380ce
Yan <nslr@riseup.net>
2019-02-12 16:24:21 +0100

    Update Status.java

--- src/main/java/com/lpdm/msorder/model/order/Status.java
+++ src/main/java/com/lpdm/msorder/model/order/Status.java
@@ -2,7 +2,7 @@ package com.lpdm.msorder.model.order;
 
 public enum Status {
 
-    CART(1), VALIDATED(2) , PAID(3), PROCESSED(4), IN_DELIVERING(5), DELIVERED(6);
+    CART(1), VALIDATED(2) , PAID(3), PROCESSED(4), IN_DELIVERING(5), DELIVERED(6), CANCELLED(7);
 
     private int id;
 
